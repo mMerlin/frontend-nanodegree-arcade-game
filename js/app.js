4f3cdbd4dc36722a896dfff2c96f69411d59f028
H. Phil Duby <philduby@phriendly.net>
2015-02-14 00:53:01 -0700

    Expand enemy constructor function to handle starting position, speed, direction Add transform functions to handle horizontally flipped tile images (the enemy bugs) Calculate proper starting coordinates for player sprite Create base input handler to move the player when input command received Add debug code to handler as convenient place to grab information while game is running Instantiate some enemies for testing position and motion logic Suppress jslint TODO: warnings until the project is closer to completion; add devel flag Add notes where comments are beyond the scope of the base project Add comments about javascript structures being used Add comments to mark the end of function definitions Add comment blocks to the prototype function definitions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-/*jslint browser: true, indent: 2, maxlen: 82 */
+/*jslint browser: true, devel: true, todo: true, indent: 2, maxlen: 82 */
 /*global Resources, ctx */
 
 /* app.js
@@ -10,18 +10,25 @@
  * Game Events:
  *  Player moves to open (unoccupied) terrain (grass or roadway)
  *  Player moves to terrain already occupied by enemy
+ *    Peform 'landedOn' enemy action: in base game, same as smash
+ *  Player moves off of playing field
+ *  Enemy collides with player sprite
+ *    Perform 'smash' enemy action
+ * Future Expansion game events:
+ *  Player moves to terrain already occupied by enemy
  *    Peform 'landedOn' enemy action (check for teeth location)
  *      Some enemies are 'all' teeth
  *  Player moves to deadly terrain (the river, or far bank)
  *    Have invisible enemy sprite, and treat as 'landedOn'
  *    Set low Z, so will land on mobile enemy first
- *  Mobile enemy collides with player sprite
- *    Perform 'smash' enemy action
- *      Variant? of 'landOn'?, but teeth always at the front
+ *    Currently N/A, since no river to cross
+ *  Player moves to terrain occupied by prize
+ *  Enemy collides with prize
  *
  * NOTE: logs are not really enemies.  The river is the enemy.  However
  *  crocodiles act the same as logs, plus they can eat a frog.  Simpler to have
  *  the log (and prizes) as enemies, but without a 'kill' method on collision.
+ *    Currently N/A, since no river to cross
  */
 
 // Wrap the application code in a function to keep it out of the global
@@ -33,18 +40,72 @@
   'use strict';
   var Enemy, Frog;
 
-  // Enemies our player must avoid
-  Enemy = function () {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  /**
+   * Reset the current transform to the identity transformation
+   *
+   * NOTE: it might be better to use an inverse tranform operation (translate
+   *   then scale), instead of setting to identity: if other processing wants
+   *   to do their own transforms.  Like a straight translate to scroll the
+   *   playing field vertically.
+   */
+  function setIdTransform() {
+    this.setTransform(1, 0, 0, 1, 0, 0);
+  }// ./function setIdTransform()
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
-    this.y = 0;
-    this.speed = 0;
-  };
+  /**
+   * Set the transform needed to flip the playing field coordinates horizontally
+   */
+  function setFlipTransform() {
+    // scale(-1, 1) === transform(-1, 0, 0, 1, 0, 0)
+    // w = 505;//Width of the playfield (colWidth*numCols)
+    // translate(-w, 0) === transform(1, 0, 0, 1, -w, 0)
+    // http://bucephalus.org/text/CanvasHandbook/CanvasHandbook.html#fn22
+    // combined => transform(-1, 0, 0, 1, -w, 0)
+    this.setTransform(-1, 0, 0, 1, 505, 0);//width of play field
+    // var calcTransform = composeTransform(
+    //   [-1, 0, 0, 1, 0, 0],
+    //   [1, 0, 0, 1, -w, 0]//testing seems to use w, not -w
+    // );
+  }// ./function setFlipTransform()
+
+  // Enemies our player must avoid.  Pseudoclassical pattern
+  Enemy = function (icon, gridRow, speed) {
+
+    // The image/sprite for the enemy instance.  This looks like (and originally
+    // was) a URL, but in this context is a key to the cached resource
+    this.sprite = icon;
+    this.row = gridRow;
+    // Pixels per second
+    this.speed = speed;
+    /* Once placed, all current enemies stay on a specific grid row.  Convert
+     * the gridRow (top (water) = 0) to a pixel position based on the grid row
+     * height, plus an offset to account for internal tile positioning
+     * differences.
+     */
+    this.y = (gridRow * 83) - 20;//rowHeight
+    this.x = -101;//Just off the edge of the playing field (-colWdith)
+    this.flip = false;
+    if (speed < 0) {
+      // need to use a horizontally flipped sprite
+      // scale(-1,1) == transform(-1,0,0,1,0,0)
+      // translate(-505,0) == transform(1,0,0,1,-505,0)//-505===field width
+      // combined => transform(-1, 0, 0, 1, -171, 0)
+      // http://bucephalus.org/text/CanvasHandbook/CanvasHandbook.html#fn22
+      this.flip = true;
+      this.speed = -this.speed;
+      //this.trnsfrm = composeTransform(
+      //  [-1, 0, 0, 1, 0, 0], [1, 0, 0, 1, -505, 0]
+      //);
+    }
+    //Engine.field never created
+    //Engine.field.rowHeight === 83;
+    //Engine.field.tileHeight - Engine.field.rowHeight
+    //  extra, but spread across top and bottom
+    /* The sprite is the same height as the field grid tiles, but the bottom
+     * padding is not the same, so need to use an offset to align with the
+     * visible grid tiles.
+     */
+  };// function Enemy(icon, gridRow, speed)
 
   // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
@@ -52,44 +113,90 @@
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    //TODO: handle 'z' too, for water and submerging turtles
-    if (!dt) { return; }//stub for jslint.
     //TODO: either properties of the instance control things like speed, or
     // this should be a super class method that invokes a subclass method
     // to handle variations.
-    this.x += this.speed * dt;
-  };
+    this.x += (this.speed * dt);
+  };// ./function Enemy.prototype.update(dt)
 
   // Draw the enemy on the screen, required method for game
   Enemy.prototype.render = function () {
+    //TODO: skip drawing if fully off the (visible) playing field
+    if (this.flip) {// flip the coordinates to display the sprite
+      setFlipTransform.call(ctx);
+    }
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  };
+    if (this.flip) {// reset to the default transfrom, so the next draw works
+      setIdTransform.call(ctx);
+    }
+  };// ./function Enemy.prototype.render()
 
-  // Now write your own player class
-  // This class requires an update(), render() and
-  // a handleInput() method.
+  // The player.  Pseudoclassical pattern
   Frog = function (icon) {
     this.sprite = icon;
-    this.x = 200;
-    this.y = 200;
-  };
+    this.x = 2 * 101;//col 2 * field.colWidth
+    this.y = (5 * 83) - 30;//row 5 * field.rowHeight
+  };// /.function Frog.constructor(icon);
 
+  /**
+   * For the current game frame, draw the player icon at the proper location on
+   * the playing field.
+   */
   Frog.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  };
+  };// ./function Frog.prototype.render()
 
+  /**
+   * For the current game frame, update the player’s position on the playing
+   * field.
+   *
+   * NOTE: for the base project, the player position never changes based on the
+   * time or frame.  It only changes when an input command is received, so there
+   * is nothing to do here.  If expand the project to get to the river crossing,
+   * then the player x position needs to be updated each frame to match the
+   * item / enemy it is riding on.  With an offset that was initially set when
+   * landing on the item.  Offset changes on command inputs.
+   */
   Frog.prototype.update = function () {
-    return undefined;//TODO: stub
-  };
+    return undefined;
+  };// ./function Frog.prototype.update()
 
-  Frog.prototype.handleInput = function () {
-    return undefined;//TODO: stub
+  /**
+   * Respond to position change commands
+   */
+  Frog.prototype.handleInput = function (cmd) {
+    var i;//DEBUG
+    switch (cmd) {
+    case 'left':
+      this.x -= 101;//field.colWidth
+      break;
+    case 'right':
+      this.x += 101;
+      break;
+    case 'up':
+      this.y -= 83;//field.rowHeight
+      break;
+    case 'down':
+      this.y += 83;//field.rowHeight
+      break;
+    }
+    //TODO: add limit checks for edge of field: die
+    // might be 'automaic', based on collision logic?
+    // DEBUG loop
+    console.log('bugs');
+    for (i = 0; i < window.allEnemies.length; i += 1) {
+      console.log(window.allEnemies[i].x);
+    }
   };
 
   // Now instantiate your objects.
   // Place all enemy objects in an array called allEnemies
   // Place the player object in a variable called player
-  window.allEnemies = [];//TODO: Stub
+  window.allEnemies = [
+    new Enemy('images/enemy-bug.png', 1, 80),
+    new Enemy('images/enemy-bug.png', 2, -40),
+    new Enemy('images/enemy-bug.png', 3, 40)
+  ];//TODO: Stub
   window.player = new Frog('images/char-boy.png');
 
 
